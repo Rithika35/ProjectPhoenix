96a17ab5b1bf7d71a9b0974f9c9ac7efad78fe92
DavidM713 <162748896+DavidM713@users.noreply.github.com>
2024-05-25 22:41:17 -0400

    Update Final_Code.ino
    removed serial prints and added touch down if statement with a poll to end the program

--- Final_Code.ino
+++ Final_Code.ino
@@ -1,20 +1,22 @@
-#include <SPI.h>
+// 1 sample rates every 6 milli checking every 100 milliseconds
+//for card reader
+//53 -> cs
+//52 -> clk
+//51 -> DI
+//50 -> DO
 #include <SD.h>
 #include <Wire.h>
 #include "SparkFunMPL3115A2.h"
 #include <math.h>
 #define NUMREADINGS 4
-File myFile;
 MPL3115A2 myPressure;
-//led pin port
-const int led_pin = 1;//LED Pin 1
+File myFile;
 
 //Timer
 unsigned long time;//once power is connected
 unsigned long timePlus;//time after liftoff
 unsigned long lastReadingTime ; //last measurement taken
 const unsigned long readingInterval = 1000000; // check sesnor every one second
-
 //Initialize temperature, pressure, and constants used to determine altitude
 float pressure = 0;
 float temperature = 0;
@@ -24,7 +26,7 @@ float  L_b = -0.0065; //Standard temperature lapse rate (K/m)
 float  R = 8.31432; //Universal gas constant (N*m/mol*K)
 float  g_0 = 9.80665; //Acceleration due to gravity (m/s^2)
 float  M = 0.0289644; //Molar mass of Earth s air (kg/mol)
-float  h_0 = 8; //Launch site elevation above sea level (2000ft / 609.6m for FAR launch site)/ orlanod is 25 m(82ft)
+float  h_0 = 11; //Launch site elevation above sea level (2000ft / 609.6m for FAR launch site)/ orlanod is 25 m(82ft)
 float P_0 = 101325;//ground pressure in orlando, must check at mojave
 float T_0 = 288;//ground temperature in kelvin
 
@@ -37,7 +39,7 @@ unsigned long LOTime; //timestamp
 // Lockoutperiod 
 int lockout = 1; //lockout = true
 float SuperSonicMin = 0.0; //numer of minutes of lockout duration
-float SuperSonicSec = 5.0; // number of seconds of lockout duration
+float SuperSonicSec = 3.0; // number of seconds of lockout duration
 const int consecutiveReadings = NUMREADINGS; // number of readings needed to exit lockout
 float lastAltitude = 0;// Variable to store the last altitude reading to check if lockout is over
 
@@ -62,6 +64,7 @@ int DroneDeploy = 0;//if solenoid valve is closed drone is ready to deploy
 float DroneDeployment = 8 ;//altitude of drone deployment in meters
 const int Actuator = 9; //Actuator pin
 int TouchDown =0;
+unsigned long Landing = 4; //altitude to terminate program
 
 //function to check if lockout is over
 bool isLockoutOver(float altitude){
@@ -91,7 +94,6 @@ bool isLockoutOver(float altitude){
 bool isApogee(float Pressure){
   static int consecutiveCount = 0; 
   bool status;
-
   // Check if altitude is greater than the last reading
   if (Pressure > ApogeePressure) {
     // Increase consecutive readings count
@@ -106,25 +108,27 @@ bool isApogee(float Pressure){
     status = false;
   }
   
-    // Update the last altitude reading
+  // Update the last altitude reading
   ApogeePressure = Pressure;
   
   return false; // Altitude is not increasing for consecutive readings
 }
-
 void setup() {
   Wire.begin();        // Join i2c bus
   myPressure.begin(); // Get sensor online
   Serial.begin(115200);
   while(!Serial);
   Serial.println("Initializing SD card...");
-  pinMode(10, OUTPUT);// change this pin to 53 for mega
-  if (!SD.begin(10)) {
+  pinMode(53, OUTPUT);// change this pin to 53 for mega
+  if (!SD.begin(53)) {
     Serial.println("initialization failed!");
     return;
-  }
-  Serial.println("initialization done.");
-  Serial.println("Welcome to Phoenix");
+  }Serial.println("initialization done.");
+  myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card  
+    if (myFile) {
+      myFile.println("Welcome to Phoenix");
+      myFile.close();//Close file
+    }
   myPressure.setModeAltimeter(); // Measure altitude above sea level in meters
   myPressure.setModeBarometer(); // Measure pressure in Pascals from 20 to 110 kPa
   myPressure.setOversampleRate(128); // Set Oversample to the recommended 128
@@ -133,7 +137,6 @@ void setup() {
   pinMode(SOLENOID2, OUTPUT);//set solenoid 2 pin
   pinMode(IGNITER, OUTPUT);//set initiator pin
   pinMode(Actuator, OUTPUT);//set initiator pin
-  pinMode(led_pin, OUTPUT);//set initiator pin
 }
 void loop() {
   // put your main code here, to run repeatedly:
@@ -147,7 +150,6 @@ void loop() {
   int TenthSec = (time/100000) % 10;
   int OneSec = (time/ 1000000) % 10;
   int TenSec = (time/ 10000000)%6;
-
   // Read sensor every 10 milliseconds
   if (time - lastReadingTime >= readingInterval) {
     lastReadingTime = time; 
@@ -156,11 +158,9 @@ void loop() {
     if(liftoff == 0){
       temperature = myPressure.readTempF();
       pressure = myPressure.readPressure();
-      Altitude_AGL =(((T_0)/(L_b))*(pow((pressure/P_0),((-R*L_b)/(g_0*M)))-1)) - h_0; //Formula to detect altitude
+      Altitude_AGL =(((T_0)/(L_b))*(pow((pressure/P_0),((-R*L_b)/(g_0*M)))-1)) - h_0; //Formula to detect altitude 
       myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card  
-      if (myFile) {
-        
-        Serial.println("Writing to Phoenix.txt...");
+      if (myFile) {      
         myFile.print("T-");myFile.print(time/60000000);myFile.print(":");myFile.print(TenSec);myFile.print(OneSec);myFile.print(":");myFile.print(TenthSec);myFile.print(HundrethSec);myFile.print(MilliSec);myFile.print(HundredMicro);myFile.print(TenMicro);myFile.print(Micro);
         myFile.print(" Pressure(Pa):");
         myFile.print(pressure, 2);
@@ -169,32 +169,15 @@ void loop() {
         myFile.print(" Altitude(m):");
         myFile.println(Altitude_AGL, 2);
         myFile.close();//Close file
-        } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-        }
-      Serial.print("T-");Serial.print(time/60000000);Serial.print(":");Serial.print(TenSec);Serial.print(OneSec);Serial.print(":");Serial.print(TenthSec);Serial.print(HundrethSec);Serial.print(MilliSec);Serial.print(HundredMicro);Serial.print(TenMicro);Serial.print(Micro);
-      Serial.print(" Pressure(Pa):");
-      Serial.print(pressure, 2);
-      Serial.print(" Temp(f):");
-      Serial.print(temperature, 2); 
-      Serial.print(" Altitude(m):");
-      Serial.println(Altitude_AGL, 2);
+      }
     }
-
     //check for liftoff
     if(time>3000000 && Altitude_AGL >= threshold && liftoff == 0 ) {
       myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
       if (myFile) {
-        Serial.println("Writing to Phoenix.txt...");
-        myFile.print("LIFT OFF DETECTED");
+        myFile.println("LIFT OFF DETECTED");
         myFile.close();//Close file
-        } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-        }
-      Serial.println("Lift-off detected!");
-      PORTB ^= (1 << led_pin);
+      }
       LOTime = time;
       liftoff = 1;
     }
@@ -206,11 +189,10 @@ void loop() {
       int OneSec = (LiftOffTime/ 1000000) % 10;
       int TenSec = (LiftOffTime/ 10000000)%6;
       pressure = myPressure.readPressure();
-      temperature = myPressure.readTempF();
+      temperature = myPressure.readTempF();   
       Altitude_AGL =(((T_0)/(L_b))*(pow((pressure/P_0),((-R*L_b)/(g_0*M)))-1)) - h_0; //Formula to detect altitude 
       myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
       if (myFile) {
-        Serial.println("Writing to Phoenix.txt...");
         myFile.print("T+");myFile.print(LiftOffTime/60000000);myFile.print(":");myFile.print(TenSec);myFile.print(OneSec);myFile.print(":");myFile.print(TenthSec);myFile.print(HundrethSec);myFile.print(MilliSec);myFile.print(HundredMicro);myFile.print(TenMicro);myFile.print(Micro);
         myFile.print(" Pressure(Pa):");
         myFile.print(pressure, 2);
@@ -220,46 +202,32 @@ void loop() {
         myFile.print(Altitude_AGL, 2);
         myFile.println(" LOCKOUT PERIOD");
         myFile.close();//Close file
-        } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-        }
-      Serial.print("T+");Serial.print(LiftOffTime/60000000);Serial.print(":");Serial.print(TenSec);Serial.print(OneSec);Serial.print(":");Serial.print(TenthSec);Serial.print(HundrethSec);Serial.print(MilliSec);Serial.print(HundredMicro);Serial.print(TenMicro);Serial.print(Micro);
-      Serial.print(" Pressure(Pa):");
-      Serial.print(pressure, 2);
-      Serial.print(" Temp(f):");
-      Serial.print(temperature, 2);
-      Serial.print(" Altitude(m):");
-      Serial.print(Altitude_AGL, 2);
-      Serial.println(" LOCKOUT PERIOD");
-      
+        }      
       if( LiftOffTime > SuperSonicCount){//if simulated lockout period is over  
         if (isLockoutOver(Altitude_AGL)) {//begin to check for 10 consectutive increasing values
           myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
           if (myFile) {
-            
-            Serial.println("Writing to Phoenix.txt...");
+
             myFile.println("Lockout Period Over");
             myFile.close();//Close file
-          } else {
-            // if the file didn't open, print an error:
-            Serial.println("error opening test.txt");
           }
-          Serial.println("Lockout Period Over");
           lockout = 0;
-          PORTB ^= (1 << led_pin);
         }
       }
     }
     
     //if lockout period is over startlooking for apogee
     if(liftoff == 1 && lockout == 0 && Apogee ==0 ){
+      LiftOffTime = time - LOTime;
+      int HundrethSec = (LiftOffTime/10000) % 10;
+      int TenthSec = (LiftOffTime/100000) % 10;
+      int OneSec = (LiftOffTime/ 1000000) % 10;
+      int TenSec = (LiftOffTime/ 10000000)%6;
       temperature = myPressure.readTempF();
-      pressure = myPressure.readPressure();
+      pressure = myPressure.readPressure();    
       Altitude_AGL =(((T_0)/(L_b))*(pow((pressure/P_0),((-R*L_b)/(g_0*M)))-1)) - h_0; //Formula to detect altitude 
       myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
       if (myFile) {
-         Serial.println("Writing to Phoenix.txt...");
         myFile.print("T+");myFile.print(LiftOffTime/60000000);myFile.print(":");myFile.print(TenSec);myFile.print(OneSec);myFile.print(":");myFile.print(TenthSec);myFile.print(HundrethSec);myFile.print(MilliSec);myFile.print(HundredMicro);myFile.print(TenMicro);myFile.print(Micro);
         myFile.print(" Pressure(Pa):");
         myFile.print(pressure, 2);
@@ -268,35 +236,18 @@ void loop() {
         myFile.print(" Altitude(m):");
         myFile.println(Altitude_AGL, 2);
         myFile.close();//Close file
-      } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-        }
-      Serial.print("T+");Serial.print(LiftOffTime/60000000);Serial.print(":");Serial.print(TenSec);Serial.print(OneSec);Serial.print(":");Serial.print(TenthSec);Serial.print(HundrethSec);Serial.print(MilliSec);Serial.print(HundredMicro);Serial.print(TenMicro);Serial.print(Micro);
-      Serial.print(" Pressure(Pa):");
-      Serial.print(pressure, 2);
-      Serial.print(" Temp(f):");
-      Serial.print(temperature, 2);
-      Serial.print(" Altitude(m):");
-      Serial.println(Altitude_AGL, 2);
-      
+      }
       if(isApogee(pressure)){
         myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
         if (myFile) {
-         Serial.println("Writing to Phoenix.txt...");
           myFile.println("Apogee Detected");
           myFile.close();//Close file
-        } else {
-          // if the file didn't open, print an error:
-          Serial.println("error opening test.txt");
         }
         Apogee = 1;
-        PORTB ^= (1 << led_pin);
-        Serial.println("Apogee Detected");
         digitalWrite(SOLENOID1, HIGH);
         digitalWrite(SOLENOID2, HIGH);
         TurnOffValve = 1;
-        
+        ApogeeTime = LiftOffTime;
         if (!igniterOn) {
           // Turn on igniter
           digitalWrite(IGNITER, HIGH);
@@ -308,16 +259,21 @@ void loop() {
    
     //After apogee wait 45 seconds to turn of solenoid, and open actuator at 400 ft/121.92 meters
     if(Apogee == 1 && liftoff==1 && lockout ==0){ 
-      ApogeeTime = time - LOTime;
+      LiftOffTime = time - LOTime;
+      int HundrethSec = (LiftOffTime/10000) % 10;
+      int TenthSec = (LiftOffTime/100000) % 10;
+      int OneSec = (LiftOffTime/ 1000000) % 10;
+      int TenSec = (LiftOffTime/ 10000000)%6;  
       temperature = myPressure.readTempF();
       pressure = myPressure.readPressure();
-      Altitude_AGL =(((T_0)/(L_b))*(pow((pressure/P_0),((-R*L_b)/(g_0*M)))-1)) - h_0; //Formula to detect altitude  
-      AfterApogee = ApogeeTime - LiftOffTime;
+      Altitude_AGL =(((T_0)/(L_b))*(pow((pressure/P_0),((-R*L_b)/(g_0*M)))-1)) - h_0; //Formula to detect altitude 
+      
+      digitalWrite(IGNITER, LOW);//turn off ignitor pin
+      AfterApogee = LiftOffTime - ApogeeTime;
       int solenoidtimeO = (AfterApogee/ 1000000) % 10;
       int solenoidtimeT = (AfterApogee/ 10000000) % 10;
       myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
       if (myFile) {
-        Serial.println("Writing to Phoenix.txt...");
         myFile.print("T+");myFile.print(LiftOffTime/60000000);myFile.print(":");myFile.print(TenSec);myFile.print(OneSec);myFile.print(":");myFile.print(TenthSec);myFile.print(HundrethSec);myFile.print(MilliSec);myFile.print(HundredMicro);myFile.print(TenMicro);myFile.print(Micro);
         myFile.print(" Pressure(Pa):");
         myFile.print(pressure, 2);
@@ -329,57 +285,39 @@ void loop() {
         myFile.print(solenoidtimeT);
         myFile.println(solenoidtimeO);
         myFile.close();//Close file
-      } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
       }
-      Serial.print("T+");Serial.print(LiftOffTime/60000000);Serial.print(":");Serial.print(TenSec);Serial.print(OneSec);Serial.print(":");Serial.print(TenthSec);Serial.print(HundrethSec);Serial.print(MilliSec);Serial.print(HundredMicro);Serial.print(TenMicro);Serial.print(Micro);
-      Serial.print(" Pressure(Pa):");
-      Serial.print(pressure, 2);
-      Serial.print(" Temp(f):");
-      Serial.print(temperature, 2);
-      Serial.print(" Altitude(m):");
-      Serial.print(Altitude_AGL, 2);
-      Serial.print(" | Descending Time:");
-      Serial.print(solenoidtimeT);
-      Serial.println(solenoidtimeO);
-      
-      digitalWrite(IGNITER, LOW);//turn off ignitor pin
-           
       if((TurnOffValve == 1) && (DrainTime < AfterApogee)){
-        myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
+         myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
         if (myFile) {
-          Serial.println("Writing to Phoenix.txt...");
           myFile.println("Turn off pumps and close solenoid valve");
           myFile.close();//Close file
-        } else {
-          // if the file didn't open, print an error:
-          Serial.println("error opening test.txt");
         }
-        Serial.println("Setting solenoid pin to LOW");
         digitalWrite(SOLENOID1, LOW);
         digitalWrite(SOLENOID2, LOW);
-        PORTB ^= (1 << led_pin);
         TurnOffValve = 0; // solenoid and pumps have been turned off
         DroneDeploy = 1;//Ready to search drone deployment altitude
       }
-
       if((Altitude_AGL < DroneDeployment) && (DroneDeploy ==1)){
-        PORTB ^= (1 << led_pin);
         myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
         if (myFile) {
-          Serial.println("Writing to Phoenix.txt...");
           myFile.println("Deploying drone...");
           myFile.close();//Close file
-        } else {
-          // if the file didn't open, print an error:
-          Serial.println("error opening test.txt");
-        }
+        } 
         digitalWrite(Actuator, HIGH);
-        Serial.println("Drone Deployed");
+
         DroneDeploy = 0;
         TouchDown = 1;
+      }      
+      if((Altitude_AGL < Landing) && (TouchDown ==1)){
+        myFile = SD.open("Phoenix.txt", FILE_WRITE); //open file on SD card
+        if (myFile) {
+          myFile.println("Welcome home Phoenix!");
+          myFile.close();//Close file
+        } 
+        TouchDown = 0;
+        while(1){};
       }
     }
   }
+
 }
